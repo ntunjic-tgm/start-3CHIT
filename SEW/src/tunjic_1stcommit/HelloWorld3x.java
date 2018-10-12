34b9273cd419e4dad2a674725bf946caf424aacc
Niko <Niko@Tunjic-Notebook>
2018-10-12 12:25:09 +0200

    2nd commit

---
+++ SEW/src/tunjic_1stcommit/HelloWorld3x.java
@@ -0,0 +1,10 @@
+package tunjic_1stcommit;
+
+public class HelloWorld3x {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
